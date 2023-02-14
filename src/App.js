5554359117300929e19d974dbaf373b4504950f9
DSankaram <sankaram@octaloop.com>
2023-02-14 17:42:37 +0530

    css on react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-       <Job salary= {9000} position="Senior SDE" company="Amazon"/>
-        <Job salary= {10000} position="Junior SDE" company="Google"/>
-        <Job salary= {90000} position="Manager" company="Netflix"/>
+       <h1 className="name">San.D</h1>
     </div>
   );
 }
-
-//using props we can pass everything to another component.
-const Job = (props) => {
-    return (
-  <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
-  </div>
-    )
-}
-
 export default App;
