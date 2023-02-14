421675508b18cdbad5790793129e9aec5dbe5674
DSankaram <sankaram@octaloop.com>
2023-02-14 18:44:58 +0530

    Ternary Operator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
 function App() {
+    const age = 17;
+    const isGreen = true;
+
   return (
+      //Ternary  Operator
     <div className="App">
-       <h1 className="name">San.D</h1>
+        {age <= 18 ? <h1> Under Age </h1> : <h1> Over Age</h1>}
+        <h1 style={{color: isGreen ? "green" :"red"}}>This has color</h1>
     </div>
   );
 }
