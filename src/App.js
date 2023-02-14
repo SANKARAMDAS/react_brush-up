17f86aa142c76a6a713de41adeab1170698305dc
DSankaram <sankaram@octaloop.com>
2023-02-14 20:01:40 +0530

    list/array[{Object}] -Exercise

--- src/App.js
+++ src/App.js
@@ -2,29 +2,18 @@ import "./App.css";
 import { User } from "./User";
 
 function App() {
-    //list/array
-    // const names = ['San.D','Pedro','Jessica','Jerry','Tom','Jack'];
-    // return <div className="App">
-    //     {names.map((name,key) => (
-    //         <h1>
-    //             {name}
-    //         </h1>
-    //     ))}
-    // </div>
-    const users = [
-        { name: 'Jack', age: 21},
-        { name: 'Jerry', age: 12},
-        { name: 'Tom', age: 23},
-    ];
+    const planets = [
+        { name: "Mars", isGasPlanet: false},
+        { name: "Earth", isGasPlanet: false},
+        { name: "Jupiter", isGasPlanet: true},
+        { name: "Venus", isGasPlanet: false},
+        { name: "Neptune", isGasPlanet: true},
+        { name: "Uranus", isGasPlanet: true},
+    ]
 
     return <div className="App">
-        {users.map((user, key) => {
-            return (
-                <User name={user.name} age={user.age}/>
-            )
-        })}
+        {planets.map((planet,key) => !planet.isGasPlanet && <h1>{planet.name}</h1>)}
     </div>
 }
-
 
 export default App;
