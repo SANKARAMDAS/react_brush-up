39bf5c9962fbeb88fd9b39928ad2072af7895e31
DSankaram <sankaram@octaloop.com>
2023-02-14 17:15:59 +0530

    using Props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 function App() {
   return (
     <div className="App">
-        <Component />
-        <Component />
-        <Component />
+        <Component name="San.D" age={21} email="testing1@abc.com"/>
+        <Component name="SanD" age={20} email="testing2@abc.com"/>
+        <Component name="Sand" age={22} email="testing3@abc.com"/>
     </div>
   );
 }
 
-const Component = () => {
+//using props we can pass everything to another component.
+const Component = (props) => {
    return (
        <div>
-          <h1> 20</h1>
-           <h1> San.D</h1>
-           <h1> sand@xyz.com</h1>
+          <h1> {props.name}</h1>
+           <h1> {props.age}</h1>
+           <h1> {props.email}</h1>
        </div>
    )
 }
