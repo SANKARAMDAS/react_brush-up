f209305b4717d074aa4900695d26fac4bafe714e
DSankaram <sankaram@octaloop.com>
2023-02-14 17:23:21 +0530

    using Props (Exercise)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 function App() {
   return (
     <div className="App">
-        <Component name="San.D" age={21} email="testing1@abc.com"/>
-        <Component name="SanD" age={20} email="testing2@abc.com"/>
-        <Component name="Sand" age={22} email="testing3@abc.com"/>
+       <Job salary= {9000} position="Senior SDE" company="Amazon"/>
+        <Job salary= {10000} position="Junior SDE" company="Google"/>
+        <Job salary= {90000} position="Manager" company="Netflix"/>
     </div>
   );
 }
 
 //using props we can pass everything to another component.
-const Component = (props) => {
-   return (
-       <div>
-          <h1> {props.name}</h1>
-           <h1> {props.age}</h1>
-           <h1> {props.email}</h1>
-       </div>
-   )
+const Job = (props) => {
+    return (
+  <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+  </div>
+    )
 }
 
 export default App;
