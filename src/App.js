7f54795e3c9f928f8db3b3970ad1a77271250ff7
DSankaram <sankaram@octaloop.com>
2023-02-14 16:09:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Component />
+        <Component />
+        <Component />
     </div>
   );
+}
+
+const Component = () => {
+   return (
+       <div>
+          <h1> 20</h1>
+           <h1> San.D</h1>
+           <h1> sand@xyz.com</h1>
+       </div>
+   )
 }
 
 export default App;
