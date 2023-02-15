38eefdba6e8b51fd60b9f5e09179d5afd2f1edec
DSankaram <sankaram@octaloop.com>
2023-02-16 01:05:29 +0530

    Customised hooks

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ import {Contact} from "./pages/Contact";
 import {Navbar} from "./Navbar";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Form} from "./component/Form";
+import {useToggle} from "./useToggle";
 
 function App() {
+    const [isVis, toggle] = useToggle();
+    const [isVis2, toggle2] = useToggle();
     return (
         <div className= "App">
-            <Form />
+            <button onClick={toggle}>
+                {isVis ? 'Hide' : 'Show'}
+            </button>
+            {isVis && <h1>Hide Text</h1>}
+
+            <button onClick={toggle2}>
+                {isVis2 ? 'Hide' : 'Show'}
+            </button>
+            {isVis2 && <h1>Hide Text</h1>}
         </div>
     )
 }
