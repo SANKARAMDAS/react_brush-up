32507abe83b61de26776e914a5a91848a32b3442
DSankaram <sankaram@octaloop.com>
2023-02-15 16:26:37 +0530

    Fetching Data from API's- Example-1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import "./App.css";
 import { User } from "./User";
-import { useState} from "react";
+import {useEffect, useState} from "react";
 import { Text} from "./Text";
+import Axios from "axios";
 
 function App() {
-    const [showText, setShowText] = useState(false);
-    return (<div className="App">
-        <button onClick={() => {
-        setShowText(!showText);
-        }}>
-            Press</button>
-        {showText && <Text />}
-    </div>)
+    const [catFact, setCatFact] = useState("");
+    // fetch("https://catfact.ninja/fact")
+    //     .then((res) => res.json()).then((data) => {
+    //     console.log(data);
+    // });
+    const factCheck = () => {
+        Axios.get("https://catfact.ninja/fact").then((res) => {
+            setCatFact(res.data.fact);
+        });
+    };
+    useEffect(() => {
+        return () => {
+           factCheck();
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <button onClick={factCheck}> Generate Cat Fact </button>
+            <p>{catFact}</p>
+        </div>
+    )
 }
 
 export default App;
