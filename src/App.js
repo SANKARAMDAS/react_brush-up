ddabfea7da5d289615f18ee4d5e18069404372f9
DSankaram <sankaram@octaloop.com>
2023-02-16 00:42:24 +0530

    Forms in React

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import "./App.css";
 import { User } from "./User";
 import {useEffect, useState, createContext} from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link,} from "react-router-dom";
 import {HomePage} from "./pages/Home";
 import {Profile} from "./pages/Profile";
 import {Contact} from "./pages/Contact";
 import {Navbar} from "./Navbar";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Form} from "./component/Form";
 
-export const AppContext = createContext();
 function App() {
-   const client = new QueryClient({
-       defaultOptions: {
-           queries: {
-               refetchOnWindowFocus: false,
-           }
-       }
-   });
     return (
         <div className= "App">
-            <QueryClientProvider client={client}>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<HomePage />}/>
-                    <Route path='/profile' element={<Profile />}/>
-                    <Route path='/contact' element={<Contact />}/>
-                    <Route path='*' element={<h1>The page you are looking for not found.</h1>}/>
-                </Routes>
-            </Router>
-            </QueryClientProvider>
+            <Form />
         </div>
     )
 }
