e27b05ae0698576ef29482f0e0d928b2255ebe12
DSankaram <sankaram@octaloop.com>
2023-02-17 12:15:13 +0530

    Type Safety- PropTypes

--- src/App.js
+++ src/App.js
@@ -9,21 +9,18 @@ import {Navbar} from "./Navbar";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Form} from "./component/Form";
 import {useToggle} from "./useToggle";
+import {Person} from "./component/Person";
 
 function App() {
-    const [isVis, toggle] = useToggle();
-    const [isVis2, toggle2] = useToggle();
     return (
-        <div className= "App">
-            <button onClick={toggle}>
-                {isVis ? 'Hide' : 'Show'}
-            </button>
-            {isVis && <h1>Hide Text</h1>}
-
-            <button onClick={toggle2}>
-                {isVis2 ? 'Hide' : 'Show'}
-            </button>
-            {isVis2 && <h1>Hide Text</h1>}
+        <div className="App">
+            <Person
+               name="San.D"
+               email="sand@xyz.com"
+               age={21}
+               isMarried={false}
+               friends={["jessica","jake","jerry","jasmine"]}
+            />
         </div>
     )
 }
